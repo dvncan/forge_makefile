1843f8a1d3e0a38281fb3a5fd4329acf9431db04
dvncan <44993570+dvncan@users.noreply.github.com>
2025-02-06 14:38:35 -0500

    Create deploy.s.sol
    Sample deploy script for working with a custom contract

---
+++ script/deploy.s.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {Script, console} from "forge-std/Script.sol";
+# import {Contract} from "../src/path";
+
+
+contract DeploySc is Script {
+
+# declare global contract here
+# Contract myContract;
+
+    function run() public {
+        vm.broadcast();
+        # myContract = new Contract(constructor_args);
+        # uint256 returnNumber = myContract.myTransaction_1(args);
+        # address returnAddress = myContract.myTransaction_2(args);
+        vm.stopBroadcast();
+    }
+}
